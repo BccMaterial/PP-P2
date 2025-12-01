b8ffad269cd382a06e5d48a1a53b3e4cf8e7e44a
CaioCollino <104874711+Resutemon1@users.noreply.github.com>
2025-11-30 22:04:07 -0300

    Update diagnostico.pl

--- diagnostico.pl
+++ diagnostico.pl
@@ -1,40 +1,89 @@
-%falha_possivel(Componente, Falha): Encontra a falha base(sintoma ou hierarquia).
+:- module(diagnostico, [
+    falha_inferida/2,
+    diagnostico/3,
+    causa_raiz/2,
+    explicacao/3,
+    por_que/3,
+    arvore_diagnostico/2
+]).
 
-% Caso 1: Falha baseada em sintoma
-falha_possivel(Componente, Falha) :-
-    sintoma_observado(Componente, Sintoma),
+:- use_module(equipamentos).
+:- use_module(sintomas).
+
+taxa_propagacao_confianca(0.8).
+
+%falhas possiveis
+falha_possivel(Comp, Falha) :-
+    sintoma_observado(Comp, Sintoma),
     relacao_sintoma_falha(Sintoma, Falha, _).
-
-% Caso 2: Falha propagada hierarquicamente
 falha_possivel(Pai, Falha) :-
     componente(Pai, Filho),
     falha_possivel(Filho, Falha).
-
-
-% Auxiliar: falha_causada(FalhaBase, FalhaFinal)
-falha_causada(FalhaBase, Falha) :-
-    Falha = FalhaBase.
-
-falha_causada(FalhaBase, Falha) :-
-    causa_indireta(FalhaBase, Falha).
-
-
-% Princiapl predicado de consulta: combina hierarquia e causalidade
-falha_inferida(Componente, FalhaFinal) :-
-    falha_possivel(Componente, FalhaBase),
-    falha_causada(FalhaBase, FalhaFinal).
-
-
-% causa_indireta(F1, F2): Rastreia a cadeia de causas(fecho transitivo).
 causa_indireta(F1, F2) :-
-    causa(F1, F2, _). % Causa direta
-
+    causa(F1, F2, _).
 causa_indireta(F1, F3) :-
     causa(F1, F2, _),
-    causa_indireta(F2, F3). % Causa indireta
+    causa_indireta(F2, F3).
+falha_causada(F, F).
+falha_causada(Base, Final) :-
+    causa_indireta(Base, Final).
 
+falha_inferida(Comp, FalhaFinal) :-
+    falha_possivel(Comp, FalhaBase),
+    falha_causada(FalhaBase, FalhaFinal).
 
-% Encontra a FalhaRaiz(falha que não é causada por outra).
-causa_raiz(Maquina, FalhaRaiz) :-
-    falha_inferida(Maquina, FalhaRaiz),
-    \+ causa_indireta(_, FalhaRaiz).
+%diagnostico
+
+diagnostico(Comp, Falha, Conf) :-
+    sintoma_observado(Comp, S),
+    relacao_sintoma_falha(S, Falha, Conf).
+
+diagnostico(Pai, Falha, NovaConf) :-
+    componente(Pai, Filho),
+    diagnostico(Filho, Falha, CBase),
+    taxa_propagacao_confianca(T),
+    NovaConf is CBase * T.
+    
+%explicações
+explicacao(Maquina, Falha, Txt) :-
+    falha_inferida(Maquina, Falha),
+    coletar_sintomas(Maquina, S),
+    format(atom(Txt),
+           'Falha (~w) deduzida por sintomas: ~w.',
+           [Falha, S]).
+
+por_que(Maquina, Falha, Just) :-
+    falha_inferida(Maquina, Falha),
+    coletar_sintomas(Maquina, Sintomas),
+
+    findall((S, F, C),
+        (member(S, Sintomas), relacao_sintoma_falha(S, F, C)),
+        Relacoes),
+
+    findall(C, causa(C, Falha, _), Causas),
+
+    format(atom(J1), 'Sintomas: ~w. ', [Sintomas]),
+    format(atom(J2), 'Relações: ~w. ', [Relacoes]),
+    format(atom(J3), 'Causas possíveis: ~w.', [Causas]),
+    atom_concat(J1, J2, T),
+    atom_concat(T, J3, Just).
+
+%arvore de diagnostico
+formata(Comp, Falha, Conf, Linha) :-
+    P is round(Conf*100),
+    format(atom(Linha), '- ~w: ~w (~w%)', [Comp, Falha, P]).
+
+arvore_diagnostico(Maq, Saida) :-
+
+    findall(C, (Maq=C ; subcomponente(Maq,C)), Lista),
+
+    findall(
+        L,
+        ( member(Comp, Lista),
+          diagnostico(Comp, F, Conf),
+          formata(Comp, F, Conf, L)
+        ),
+        Linhas
+    ),
+
+    atomic_list_concat([Maq,'\n--- DIAGNÓSTICOS ---'|Linhas], '\n', Saida).
