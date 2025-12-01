03a9c1e9ef54ec0f6e6e7476b1f586b101befd37
CaioCollino <104874711+Resutemon1@users.noreply.github.com>
2025-11-30 22:00:18 -0300

    Update acoes.pl

--- acoes.pl
+++ acoes.pl
@@ -1,11 +1,6 @@
-% Base de fatos
-acao_corretiva(superaquecimento, 'Verificar sistema de refrigeração e nível de óleo.').
-acao_corretiva(baixa_pressao_oleo, 'Verificar bomba de óleo e nível do reservatório.').
-acao_corretiva(curto_circuito, 'Isolar o circuito de controle e inspecionar fiação.').
-acao_corretiva(sensor_inoperante, 'Calibrar ou substituir sensor.').
-acao_corretiva(vibracao_excessiva, 'Verificar balanceamento e fixação de componentes.').
-acao_corretiva(parada_inesperada, 'Revisão geral do sistema elétrico e mecânico - URGENTE.').
-acao_corretiva(eixo_desalinhado, 'Realinhar eixo de rotação.').
+:- module(acoes, [
+    recomendar_acao/4
+]).
 
 % Mapeamento da severidade para a prioridade
 mapear_severidade_para_prioridade(alta, urgente).
